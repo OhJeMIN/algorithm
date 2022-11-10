6a26643f4b6d57e406e1a9ea8c7c554339020615
MunSeoHee <nansh9815@naver.com>
2022-11-10 09:53:21 +0900

    [PGS] 모스부호 (1) / Lv0 / 3m

---
+++ Programmers/코딩테스트 입문/모스부호 (1)/MunSeoHee.py
@@ -0,0 +1,14 @@
+def solution(letter):
+    answer = ""
+    morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    }
+    letter = letter.split(" ")
+    
+    for morse_string in letter :
+        answer += morse[morse_string]
+    return answer
