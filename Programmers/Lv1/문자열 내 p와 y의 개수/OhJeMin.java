77331ab653a9ad852c101ab2aabb41cbd0be942b
ohJeMin <61536578+OhJeMIN@users.noreply.github.com>
2022-12-24 22:11:11 +0900

    [PGS] 문자열 내 P와 Y의 개수 Lv1 Refactor

--- Programmers/Lv1/문자열 내 p와 y의 개수/OhJeMin.java
+++ Programmers/Lv1/문자열 내 p와 y의 개수/OhJeMin.java
@@ -1,10 +1,20 @@
+
+import java.util.Stack;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        int Pnum =0 ,Ynum =0;
-        Pnum = s.length() - s.replace("p", "").length() - s.replace("P", "").length();
-        Ynum = s.length() - s.replace("y", "").length() - s.replace("Y", "").length();
-        if(Pnum != Ynum) answer = false;
+        int Pnum =0, Ynum = 0;
+        Stack<String> stack = new Stack<>();
+        String[] strArray = s.toLowerCase().split("");        
+        for(int i = 0; i< s.length(); i++){            
+            stack.push(strArray[i]);
+        }
+        for(int i = 0; i < s.length(); i++){  
+            String popedFromStack = stack.pop();
+            if(popedFromStack.equals("p"))Pnum +=1;
+            else if(popedFromStack.equals("y"))Ynum +=1;
+        }
+        if(Ynum != Pnum) answer = false;
         return answer;
     }
 }
