a5d18ba02d4879b29e733f56f35d7d2303f3eb46
MunSeoHee <nansh9815@naver.com>
2022-12-27 21:02:29 +0900

    Update MunSeoHee.py

--- Programmers/Lv1/하샤드 수/MunSeoHee.py
+++ Programmers/Lv1/하샤드 수/MunSeoHee.py
@@ -1,12 +1,5 @@
 def solution(x):
-    num = x
     sum = 0
-    
-    while (num > 0) :
-        sum += num % 10
-        num = num // 10
-    
-    if x % sum == 0 :
-        return True
-    else :
-        return False
+    for i in str(x) :
+        sum += int(i)
+    return x % sum == 0
