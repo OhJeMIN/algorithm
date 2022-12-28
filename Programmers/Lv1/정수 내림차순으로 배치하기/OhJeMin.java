aaf0d7b57f499c33e72b3a2cf4312935939a5e14
ohJeMin <61536578+OhJeMIN@users.noreply.github.com>
2022-12-28 21:43:37 +0900

    refactor

--- Programmers/Lv1/정수 내림차순으로 배치하기/OhJeMin.java
+++ Programmers/Lv1/정수 내림차순으로 배치하기/OhJeMin.java
@@ -2,12 +2,9 @@ import java.util.*;
 class Solution {
     public long solution(long n) {
         String[] str = Long.toString(n).split("");
-        Arrays.sort(str, Collections.reverseOrder());
-        
+        Arrays.sort(str, Collections.reverseOrder());        
         String strAnswer = "";
-        for(String string : str) strAnswer += string;
-        
-        return Long.parseLong(strAnswer);
-        
+        for(String string : str) strAnswer += string;        
+        return Long.parseLong(strAnswer);        
     }
 }
