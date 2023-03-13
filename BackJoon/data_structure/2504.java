2dfaabdc2c1f48cef32952f1182e772f2ba124bd
ohJeMin <61536578+OhJeMIN@users.noreply.github.com>
2023-03-13 19:07:17 +0900

    [REFOCTOR] 오류 수정
    에러 원인
    1.  EmptyStack 오류
    2.  ))))로 들어왔을때 오류 처리
    3.  stack.isEmpty 오류 처리 안함
    4.  오류 처리 wrong의 변수 처리 int -> boolean으로 변경

--- BackJoon/data_structure/2504.java
+++ BackJoon/data_structure/2504.java
@@ -1,7 +1,8 @@
+package org.example;
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class backJoon2504 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split("");
@@ -9,7 +10,8 @@ public class Main {
     }
     public static void calculator(String[] input) {
         Stack<String> st = new Stack<>();
-        int result = 1, answer = 0, wrong = -1;
+        int result = 1, answer = 0;
+        Boolean wrong = true;
         String previousInput = "X";
         for (int i = 0; i < input.length; i++) {
             if(i>0) previousInput = input[i-1];
@@ -17,33 +19,49 @@ public class Main {
                 result = openBracket(input[i],st, result);
             }
             else{ // input == ) or ]
+                if(st.isEmpty()) {
+                    wrong = false;
+                    break;
+                }
                 if(input[i].equals(")")){
                     if(previousInput.equals(")") || previousInput.equals("]")){
                         result /=2;
-                        if(!st.isEmpty()) st.pop();
+                        st.pop();
+
                     }
                     else{
-                        int correct = closeBracket(input[i],previousInput,st,answer,result);
+                        int correct = closeBracket(input[i],st,answer,result);
                         if( correct>0) answer = correct;
-                        else wrong = 0;
+                        else {
+                            wrong = false;
+                            break;
+                        }
                         result /=2;
                     }
                 }
-                else{//input == ]
+                else if(input[i].equals("]")){//input == ]
                     if(previousInput.equals("]") || previousInput.equals(")")){
                         result /=3;
-                        if(!st.isEmpty()) st.pop();
+                        st.pop();
+
                     }
                     else{
-                        int correct = closeBigBracket(input[i],previousInput,st,answer,result);
+                        int correct = closeBigBracket(input[i],st,answer,result);
                         if( correct>0) answer = correct;
-                        else wrong = 0;
+                        else {
+                            wrong = false;
+                            break;
+                        }
                         result /=3;
                     }
                 }
+                else{
+                    wrong = false;
+                    break;
+                }
             }
         }
-        if(wrong == 0) System.out.println(wrong);
+        if(!wrong || !st.isEmpty()) System.out.println(0);
         else System.out.println(answer);
     }
     public static int openBracket(String input, Stack<String> st, int result){
@@ -57,13 +75,13 @@ public class Main {
         }
         return result;
     }
-    public static int closeBracket(String input,String previousInput, Stack<String> st, int answer, int result){
+    public static int closeBracket(String input, Stack<String> st, int answer, int result){
         int wrong = -1;
         if(input.equals(")")){
-            if(st.isEmpty() || previousInput.equals("[")){
+            if(st.isEmpty() || !st.peek().equals("(")){
                 wrong = 0;
             }
-            if(previousInput.equals("(")) {
+            if(st.peek().equals("(")) {
                 answer +=result;
             }
         }
@@ -72,13 +90,13 @@ public class Main {
         return answer;
     }
 
-    public static int closeBigBracket(String input,String previousInput, Stack<String> st, int answer, int result){
+    public static int closeBigBracket(String input, Stack<String> st, int answer, int result){
         int wrong = -1;
         if(input.equals("]")){
-            if(st.isEmpty() || previousInput.equals("(")){
+            if(st.isEmpty() || !st.peek().equals("[")){
                 wrong = 0;
             }
-            if(previousInput.equals("[")) {
+            if(st.peek().equals("[")) {
                 answer +=result;
             }
         }
