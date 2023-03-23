da67058192260b1c0572e3078c4f6b8cd0c2b519
ohJeMin <61536578+OhJeMIN@users.noreply.github.com>
2023-03-23 12:17:31 +0900

    [REFACTOR] 정렬기준 수정
    1. 굳이 선언한 변수 삭제
    2. 조건인 만약 비교군이 절대값이 같다면 그냥 기존 숫자로 오름차순으로 절대값이 같지 않다면 절대값 기준으로 오름차순 시켜줬다

--- BackJoon/data_sturcture2/11286.java
+++ BackJoon/data_sturcture2/11286.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class backJoon11286 {
@@ -9,12 +9,15 @@ public class backJoon11286 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder st =new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> pq = new PriorityQueue<>(((o1, o2) -> {
-            int absO1 = Math.abs(o1);
-            int absO2 = Math.abs(o2);
-            if(absO1 == absO2) return o1 > o2 ? 1 : -1;
-            return absO1- absO2;
-        }));
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return Math.abs(o1) == Math.abs(o2) ? Integer.compare(o1,o2) : Integer.compare(Math.abs(o1),Math.abs(o2));
+            }
+        });
+        /*PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) ->
+                Math.abs(o1) == Math.abs(o2) ? Integer.compare(o1,o2) : Integer.compare(Math.abs(o1),Math.abs(o2))
+        );*/
         for(int i=0;i<N;i++){
             int input = Integer.parseInt(br.readLine());
             if(input == 0) {
