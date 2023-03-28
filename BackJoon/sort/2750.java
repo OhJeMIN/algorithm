9260f1fa8b3e4f46bc2da08f618eded575d8fb2a
ohJeMin <61536578+OhJeMIN@users.noreply.github.com>
2023-03-28 15:35:04 +0900

    [REFACTOR] 버블 정렬 구현
    1. N이 1000 이하이므로 시간복잡도 N^2인 버블정렬도 가능함
    2. 배열 하나하나를 읽으며 위치를 바꿔줌
    3. 배열의 비교가 끝난 맨 마지막은 가장 크다. -> 정렬되었으므로 다음 정렬 대상에서 제외시킴
    4. 대상에서 제외 시키는 조건은 N-1-i로 하나씩 뒤에서부터 제외.

--- BackJoon/sort/2750.java
+++ BackJoon/sort/2750.java
@@ -15,5 +15,25 @@ public class backJoon2750 {
         for(int i=0;i<N;i++){
             System.out.println(pq.poll());
         }
+        
+       /* 
+       //버블 정렬
+       int arr[] = new int[N];
+       for(int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        
+        for(int i=0;i<N-1;i++){ // 맨마지막에 2개가 스왑 되므로 끝까지 갈 필요 없다
+            for(int j=0;j<N-1-i;j++){ //N-1-i 루프가 돌수록 뒤에 정렬된 데이터를 제외하고 돌린다.
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for(int i=0;i<N;i++){
+            System.out.println(arr[i]);
+        }*/
     }
 }
