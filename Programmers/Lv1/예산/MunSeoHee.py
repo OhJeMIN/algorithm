9d3ddc77ef69dbd4146a2cdd271b9a75e131fb9a
MunSeoHee <nansh9815@naver.com>
2023-04-29 16:32:08 +0900

    [PGS] 예산 / Lv1

---
+++ Programmers/Lv1/예산/MunSeoHee.py
@@ -0,0 +1,9 @@
+def solution(d, budget):
+    d.sort()
+    answer = 0
+    for item in d :
+        budget -= item
+        if budget < 0 :
+            return answer
+        answer += 1
+    return answer
