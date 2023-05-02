9457098fa1340703459d18730a7bb8aaded8c1fc
ohJeMin <61536578+OhJeMIN@users.noreply.github.com>
2023-05-02 16:32:26 +0900

    [REFACTOR] 불필요한 변수 제거

--- Programmers/Lv1/서울에서 김서방 찾기/Ohjemin.py
+++ Programmers/Lv1/서울에서 김서방 찾기/Ohjemin.py
@@ -1,6 +1,4 @@
 def solution(seoul):
-    answer = ''
     for i,v in enumerate(seoul):
         if(v == "Kim"):
-            answer = "김서방은 "+ str(i) +"에 있다"
-    return answer
+            return "김서방은 "+ str(i) +"에 있다"
