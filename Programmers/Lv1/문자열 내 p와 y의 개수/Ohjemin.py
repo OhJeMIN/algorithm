725f929022608ad576de4656891df566a7a1eb04
ohJeMin <61536578+OhJeMIN@users.noreply.github.com>
2023-05-02 14:53:14 +0900

    [REFACTOR] 1m

--- Programmers/Lv1/문자열 내 p와 y의 개수/Ohjemin.py
+++ Programmers/Lv1/문자열 내 p와 y의 개수/Ohjemin.py
@@ -5,3 +5,10 @@ def solution(s):
             List[index]= value.lower();
     if(List.count('p') != List.count('y')): return False;
     return True;
+
+def solution(s):
+    answer = True
+    count_p = s.count('p')+ s.count('P');
+    count_s = s.count('y')+ s.count('Y');
+    if(count_p != count_s): return False
+    return True
