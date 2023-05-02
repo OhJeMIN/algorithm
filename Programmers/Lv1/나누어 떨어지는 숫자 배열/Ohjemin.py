7036fa9bbc5710f0705807077300da71fdcded28
ohJeMin <61536578+OhJeMIN@users.noreply.github.com>
2023-05-02 16:44:55 +0900

    [REFACTOR] sorted 변경

--- Programmers/Lv1/나누어 떨어지는 숫자 배열/Ohjemin.py
+++ Programmers/Lv1/나누어 떨어지는 숫자 배열/Ohjemin.py
@@ -4,5 +4,4 @@ def solution(arr, divisor):
         if( i % divisor == 0):
             answer.append(i);
     if(len(answer) == 0): answer.append(-1);
-    answer.sort();
-    return answer
+    return sorted(answer)
