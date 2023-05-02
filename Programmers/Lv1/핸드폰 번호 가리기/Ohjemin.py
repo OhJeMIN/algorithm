5bcec7f307c4d6cabb31fe308a51848f61524f31
ohJeMin <61536578+OhJeMIN@users.noreply.github.com>
2023-05-02 16:49:27 +0900

    [REFACTOR] string의 slice 활용

--- Programmers/Lv1/핸드폰 번호 가리기/Ohjemin.py
+++ Programmers/Lv1/핸드폰 번호 가리기/Ohjemin.py
@@ -1,8 +1,2 @@
 def solution(phone_number):
-    answer = ''
-    phone = list(phone_number);
-    for i,v in enumerate(phone_number):
-        if(i >= len(phone_number)-4):
-            answer += v;
-        else: answer += "*"
-    return answer
+    return len(phone_number[0:-4]) * '*' + phone_number[-4:]
