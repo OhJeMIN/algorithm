f582f0c33d5bbdb0ffb71f21ecc4c5b789190552
ohJeMin <61536578+OhJeMIN@users.noreply.github.com>
2023-05-04 11:37:14 +0900

    [REFACTOR] 세미콜론 제거.. ㅋㅋ

--- Programmers/Lv2/이진 변환 반복하기/Ohjemin.py
+++ Programmers/Lv2/이진 변환 반복하기/Ohjemin.py
@@ -3,7 +3,7 @@ def solution(s):
     removed_s = 0
     while len(s) > 1:
         str = ''
-        change_s+=1;
+        change_s+=1
         for i in s:
             if i == '0': 
                 removed_s+=1
