fc8be30eb5c5fa06999c73543a0c5531a9725faf
ohJeMin <61536578+OhJeMIN@users.noreply.github.com>
2023-05-04 14:13:30 +0900

    [REFACTOR] .count 로 1의 개수 구함

--- Programmers/Lv2/다음 큰 숫자/Ohjemin.py
+++ Programmers/Lv2/다음 큰 숫자/Ohjemin.py
@@ -1,18 +1,10 @@
 def solution(n):
     answer = 0
-    str_n = bin(n)[2:]
-    num = 0
-    for i in str_n:
-        if i == '1':
-            num+=1
-    while n > 0:
+    count_n = bin(n).count('1')
+    while n >0:
         n+=1
-        str_n = bin(n)[2:]
-        num_two = 0
-        for i in str_n:
-            if i == '1':
-                num_two+=1
-        if(num == num_two):
-                answer = n
-                break;
+        count_next_n = bin(n).count('1')
+        if(count_n == count_next_n):
+            answer = n
+            break
     return answer
