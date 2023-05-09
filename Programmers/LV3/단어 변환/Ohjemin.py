95dd6c5e5065294b32185f242a2aa86411f66b0c
jasonoh22 <jasonoh22@naver.com>
2023-05-09 14:47:26 +0900

    [PGS] 단어 변환 lv3
    두개의 알파벳이 같은게 아닌 한개의 알파벳만 다른것을 큐에 넣어 너비우선탐색함

--- Programmers/LV3/단어 변환/Ohjemin.py
+++ Programmers/LV3/단어 변환/Ohjemin.py
@@ -5,22 +5,19 @@ def solution(begin, target, words):
         return 0
     visited = [False for i in range(len(words))]
     queue = deque()
-    queue.append(begin)
+    queue.append([begin,0])
     while queue:
-        popped = queue.popleft()
-        if popped == target: break;
+        popped,cnt = queue.popleft()
+        if popped == target: 
+            answer = cnt
+            break;
         for idx, word in enumerate(words):
             same = 0
             contains = 0
             for i in range(len(word)):
-                if popped[i:i+1] == word[i:i+1]:
-                    same+=1
-                else:
-                    if word[i:i+1] in target:
-                        contains +=1                    
-            if same == 2 and not visited[idx] and contains == 1:
-                answer +=1
+                if popped[i:i+1] != word[i:i+1]:
+                    same+=1                                 
+            if same == 1 and not visited[idx]:
                 visited[idx] = True
-                print(word)
-                queue.append(word)
+                queue.append([word,cnt+1])
     return answer
