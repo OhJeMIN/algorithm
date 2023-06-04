e3be5c9cfea7f6655df9c39070cecd79235698e0
ohJeMin <61536578+OhJeMIN@users.noreply.github.com>
2023-06-04 15:13:29 +0900

    [REFACTOR] 프로세스 lv2 완.
    만약 우선 순위가 큰 숫자가 존재한다면 다시 큐에 넣어줬다.
    아니라면 버리고 +1 해줬다.

--- Programmers/Lv2/프로세스/Ohjemin.py
+++ Programmers/Lv2/프로세스/Ohjemin.py
@@ -1,22 +1,15 @@
-from collections import deque
 def solution(priorities, location):
-    answer = 0    
-    q = deque()
-    order = {}
-    for idx, num in enumerate(priorities):
-        q.append([idx ,num])
-    index = 0
-    for i in range(len(priorities)):
-        if priorities[index] < max(priorities):
-            num = q.popleft()
-            q.append(num)
-            index+=1
-    index = 0
-    for i in q:
-        order[i[0]] = i[1]
-    reverse_order = sorted(order.items(),key=lambda x:x[1],reverse=True)
-    for i in reverse_order:
-        answer+=1
-        if i[0] == location:
-            break;
-    return answer
+    queue =  [(i,p) for i,p in enumerate(priorities)]
+    answer = 0
+    while True:
+        cur = queue.pop(0)
+        exist = False
+        for q in queue:
+            if cur[1]< q[1]:
+                exist = True
+        if exist:
+            queue.append(cur)
+        else:
+            answer += 1
+            if cur[0] == location:
+                return answer
