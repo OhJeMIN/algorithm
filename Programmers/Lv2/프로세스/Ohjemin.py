6b35cb671b52be5ecbeca47bc847b405b73f44fc
ohJeMin <61536578+OhJeMIN@users.noreply.github.com>
2023-06-04 15:03:35 +0900

    [REFACTOR] 프로세스 lv2 에러 5개줄임

--- Programmers/Lv2/프로세스/Ohjemin.py
+++ Programmers/Lv2/프로세스/Ohjemin.py
@@ -1,16 +1,22 @@
 from collections import deque
 def solution(priorities, location):
-    answer = 0
-    order = {}    
-    q = deque() 
+    answer = 0    
+    q = deque()
+    order = {}
     for idx, num in enumerate(priorities):
-        order[idx] = order.get(idx,[]) + [num]
+        q.append([idx ,num])
+    index = 0
+    for i in range(len(priorities)):
+        if priorities[index] < max(priorities):
+            num = q.popleft()
+            q.append(num)
+            index+=1
+    index = 0
+    for i in q:
+        order[i[0]] = i[1]
     reverse_order = sorted(order.items(),key=lambda x:x[1],reverse=True)
     for i in reverse_order:
-        q.append(i)
-    for i in q:
-        print(i)
-    print(reverse_order[0][0])
-    print(q)
-    print(order)
+        answer+=1
+        if i[0] == location:
+            break;
     return answer
