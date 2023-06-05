89287e89e035d7ce6c04b4048877c0c7479a30b7
ohJeMin <61536578+OhJeMIN@users.noreply.github.com>
2023-06-05 14:55:27 +0900

    [REFACTOR] 다리를 지나는 트럭 lv2
    sum(bridge) 하면 시간복잡도O(N) 때문에 임시 변수를 통해 관리함

--- Programmers/Lv2/다리를 지나는 트럭/Ohjemin.py
+++ Programmers/Lv2/다리를 지나는 트럭/Ohjemin.py
@@ -1,12 +1,16 @@
 from collections import deque
 def solution(bridge_length, weight, truck_weights):
     answer = 0
+    q = []
     bridge = [0 for i in range(bridge_length)]
+    sum_bridge = 0
     while bridge:        
         answer += 1
-        bridge.pop(0)        
+        sum_bridge -= bridge[0]        
+        bridge.pop(0)
         if truck_weights:
-            if sum(bridge) + truck_weights[0] <= weight:            
+            if  sum_bridge + truck_weights[0] <= weight:
+                sum_bridge += truck_weights[0] 
                 t = truck_weights.pop(0)
                 bridge.append(t)
             else:
