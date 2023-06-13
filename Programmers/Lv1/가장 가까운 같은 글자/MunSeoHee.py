254a17a4619b0ba8f9095d0f439df75101c1a235
MunSeoHee <nansh9815@naver.com>
2023-06-13 17:00:51 +0900

    [REFACTOR] 가장 가까운 같은 글자

--- Programmers/Lv1/가장 가까운 같은 글자/MunSeoHee.py
+++ Programmers/Lv1/가장 가까운 같은 글자/MunSeoHee.py
@@ -1,7 +1,7 @@
 def solution(s):
     answer = []
     for index in range(0, len(s)) :
-        sub_string = "" if not s[0:index] else s[0:index]
+        sub_string = s[0:index]
         find_index = sub_string.rfind(s[index])
         if (find_index == -1) : 
             answer.append(find_index)
