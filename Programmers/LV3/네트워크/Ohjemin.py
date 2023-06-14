8b60bd887f32ea16e9ed4b1f714efb5bdd8458ea
ohJeMin <61536578+OhJeMIN@users.noreply.github.com>
2023-06-14 10:50:04 +0900

    [Update] 네트워크 lv3

--- Programmers/LV3/네트워크/Ohjemin.py
+++ Programmers/LV3/네트워크/Ohjemin.py
@@ -16,3 +16,21 @@ def solution(n, computers):
             DFS(i,computers,n)
             answer +=1
     return answer
+
+
+#2번째 풀이
+def solution(n, computers):
+    answer = 0
+    visited = [False for i in range(n)]
+    
+    def DFS(computers, n , i):
+        visited[i] = True
+        for j in range(n):
+            if computers[i][j] == 1 and not visited[j]:
+                DFS(computers, n , j)
+        
+    for i in range(n):
+        if not visited[i]:
+            DFS(computers, n, i)
+            answer +=1
+    return answer
