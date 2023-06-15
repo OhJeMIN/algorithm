cae783682de17004c0063c64bf8dccb03beee926
ohJeMin <61536578+OhJeMIN@users.noreply.github.com>
2023-06-15 09:59:40 +0900

    [PGS] 예상 대진표 lv2

--- Programmers/Lv2/예상 대진표/Ohjemin.py
+++ Programmers/Lv2/예상 대진표/Ohjemin.py
@@ -2,12 +2,12 @@ def solution(n,a,b):
     answer = 0
     winner = [0 for i in range(n+1)]
     pick = {'A': a, 'B': b}
-    while n > 1:
+    while n > 1:        
         for i in range(1, n+1, 2):
             winner[(i+1)//2] = winner[i]+1
-            if i == a or i+1 == a:
+            if i == pick['A'] or i+1 == pick['A']:
                 pick['A'] = (i+1)//2
-            if i+1 == b or i == b :
+            if i+1 == pick['B'] or i == pick['B'] :
                 pick['B'] = (i+1)//2
         n = n // 2
         if pick['A'] == pick['B']:
