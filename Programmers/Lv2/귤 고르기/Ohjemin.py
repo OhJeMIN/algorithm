f021a61bf21a5276910407285b43c518ec986ebc
ohJeMin <61536578+OhJeMIN@users.noreply.github.com>
2023-06-16 15:33:24 +0900

    [PGS] 귤 고르기 lv2

--- Programmers/Lv2/귤 고르기/Ohjemin.py
+++ Programmers/Lv2/귤 고르기/Ohjemin.py
@@ -2,10 +2,14 @@ from collections import deque
 def solution(k, tangerine):
     answer = 0
     basket = {}
-    for size in dict.fromkeys(tangerine):
-        basket[size] = basket.get(size,'') + str(tangerine.count(size))
+    for size in tangerine:
+        if size in basket:
+            basket[size] +=1
+        else:
+            basket[size] = 1
     for i in sorted(basket.items(), key = lambda x: x[1], reverse = True):
-        k -= int(i[1])
-        answer +=1
-        if k <=0:
+        if k <= 0:
             return answer
+        k -= i[1]
+        answer +=1
+    return answer
