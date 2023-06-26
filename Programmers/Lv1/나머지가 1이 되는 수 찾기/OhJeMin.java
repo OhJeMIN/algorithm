919a3ba678c770e11c89b17b7377d90920f2c05e
ohJeMin <61536578+OhJeMIN@users.noreply.github.com>
2023-06-26 14:51:08 +0900

    [REFACTOR] 나머지가 1이 되는 수 찾기 LV1

--- Programmers/Lv1/나머지가 1이 되는 수 찾기/OhJeMin.java
+++ Programmers/Lv1/나머지가 1이 되는 수 찾기/OhJeMin.java
@@ -1,14 +1,8 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        int cnt = 1;
-        while(1){
-           if( n % cnt == 1){
-               answer = cnt;
-               break;
-           }
-            cnt++;
+        for(int i =2;i<n; i++){
+            if (n % i == 1) return i;
         }
-        return answer;
+        return 0;
     }
 }
