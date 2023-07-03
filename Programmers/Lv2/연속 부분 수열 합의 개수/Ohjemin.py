5985579a903ebe70ad6dfdb6b153fa128b9e1916
ohJeMin <61536578+OhJeMIN@users.noreply.github.com>
2023-07-03 17:22:51 +0900

    [PGS] 연속 부분 수열 합의 개수 성공

--- Programmers/Lv2/연속 부분 수열 합의 개수/Ohjemin.py
+++ Programmers/Lv2/연속 부분 수열 합의 개수/Ohjemin.py
@@ -1,13 +1,11 @@
 def solution(elements):
     answer = 0
     length = len(elements)
-    arr = []
+    arr = set()
     for i in range(1,length+1):
         for j in range(length):
             if j+i > length : 
-                num = sum(elements[j:j+i]) + sum(elements[:j+i-length])
+                arr.add(sum(elements[j:j+i]) + sum(elements[:j+i-length]))
             else: 
-                num = sum(elements[j:j+i])
-            if num not in arr:
-                arr.append(num)
+                arr.add(sum(elements[j:j+i]))
     return len(arr)
