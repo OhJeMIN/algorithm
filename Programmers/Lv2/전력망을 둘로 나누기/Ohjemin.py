45cdfb4ad93a5513e061511045354da743a9f6bd
ohJeMin <61536578+OhJeMIN@users.noreply.github.com>
2023-08-16 18:47:43 +0900

    [PGS] 전력망을 둘로 나누기 lv2 실패

--- Programmers/Lv2/전력망을 둘로 나누기/Ohjemin.py
+++ Programmers/Lv2/전력망을 둘로 나누기/Ohjemin.py
@@ -1,14 +1,28 @@
 def solution(n, wires):
     answer = -1
-    visited = [False for i in range(len(wires))]
+    visited = [[False for j in range(len(wires))] for i in range(len(wires))]
     network = {}
+    networkR = {}
     for idx, n in wires:
         network[idx] = network.get(idx, []) + [n]
-        
-    #def flow():
-        
-    for idx, n in enumerate(wires):
-        for key, val in network.items():
-            flow(idx, n , key, val)
+        networkR[n] = networkR.get(n, []) + [idx]            
+    
+    def flow(idx, num, key, val, network, visited, n):
+        if idx == key && num == val:
+            return n
+        if !visited[key][val] && !visited[val][key]:
+            visited[key][val] = True
+            visited[val][key] = True
+            n-=1
+            flow(idx,num, val, network[val], network, visited, n)
                 
+        
+    for idx, num in enumerate(wires):
+        int dividedNet = 0
+        for key, val in network.items():
+            flow(idx, num, key, val, network, visited, n)
+    
+            
+    print(network)
+    print(networkR)
     return answer
