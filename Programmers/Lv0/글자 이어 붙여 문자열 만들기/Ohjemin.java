7690a54f7d28ef0b2d8261ceff42a9e6e20fe987
ohJeMin <61536578+OhJeMIN@users.noreply.github.com>
2023-11-05 12:04:46 +0900

    [PGS] 글자 이어 붙여 문자열 만들기 자바 스트림 활용

---
+++ Programmers/Lv0/글자 이어 붙여 문자열 만들기/Ohjemin.java
@@ -0,0 +1,7 @@
+import java.util.stream.*;
+import java.util.*;
+class Solution {
+    public String solution(String my_string, int[] index_list) {
+        return Arrays.stream(index_list).mapToObj(i -> String.valueOf(my_string.charAt(i))).collect(Collectors.joining());
+    }
+}
